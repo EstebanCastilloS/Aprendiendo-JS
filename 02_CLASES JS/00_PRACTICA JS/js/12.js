cf7f76a601703c1eb749fdc445ddf1f13f24db7b
EstebanCastillo <ncastillo308@unab.edu.co>
2024-01-08 13:19:56 -0500

    Iteradores en JS

---
+++ 02_CLASES JS/00_PRACTICA JS/js/12.js
@@ -0,0 +1,17 @@
+// Iteradores en JS
+
+const tecnologias = ['Vue.js', 'JavaScript', 'Node.js', 'HTML', 'CSS', 'Git']
+
+// forEach intera el arreglo
+const arrayForEach = tecnologias.forEach(function(tech) {
+    return tech
+})
+
+// map intera y genera un nuevo arreglo
+const arrayMap = tecnologias.map(function(tech) {
+    return tech
+})
+
+
+console.log(arrayForEach)
+console.log(arrayMap)
