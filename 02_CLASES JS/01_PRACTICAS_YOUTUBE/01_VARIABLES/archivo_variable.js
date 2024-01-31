e9398ea3451b24a83664e59becc1946274b2f8e0
EstebanCastillo <ncastillo308@unab.edu.co>
2024-01-30 23:40:56 -0500

    01_Variables

---
+++ 02_CLASES JS/01_PRACTICAS_YOUTUBE/01_VARIABLES/archivo_variable.js
@@ -0,0 +1,15 @@
+document.write("Iniciando con Variable <br>");
+
+// Declarar una variable
+//modo global
+var nombre = "Esteban";
+
+//modo local
+let apellido = "Garcia";
+
+//constante
+const edad = 30;
+
+document.write(nombre + "<br>");
+document.write(apellido + "<br>");
+document.write(edad + "<br>");
