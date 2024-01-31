925c4ca0cdf4d20eefb2b2cb68ede7502885f618
EstebanCastillo <ncastillo308@unab.edu.co>
2024-01-31 11:16:00 -0500

    04_FUNCIONES_MATEMÁTICAS

---
+++ 02_CLASES JS/01_PRACTICAS_YOUTUBE/04_FUNCIONES_MATEMATICAS/archivo.js
@@ -0,0 +1,62 @@
+//reondear a siguiente entero en decimal .5
+var precio = Math.round(399.53);
+document.write("El precio redondeado es: " + precio + "<br>");
+
+//redondear sin importar decimal hacia arriba
+var precio = Math.ceil(299.4);
+document.write("El precio redondeado hacia arriba es: " + precio + "<br>");
+
+//redondear sin importar decimal hacia abajo
+var precio = Math.floor(199.9);
+document.write("El precio redondeado hacia abajo es: " + precio + "<br>");
+
+//calcular el seno de un angulo
+var angulo = Math.sin(45);
+document.write("El seno de 45 es: " + angulo + "<br>");
+
+//calcular el exponencial de un numero
+var numero = Math.exp(2);
+document.write("El exponencial de 2 es: " + numero + "<br>");
+
+//calcular el logaritmo de un numero
+var numero = Math.log(10);
+document.write("El logaritmo de 10 es: " + numero + "<br>");
+
+//calcular la raiz cuadrada de un numero
+var numero = Math.sqrt(25);
+document.write("La raiz cuadrada de 25 es: " + numero + "<br>");
+
+//calcular el valor absoluto de un numero
+var numero = Math.abs(-10);
+document.write("El valor absoluto de -10 es: " + numero + "<br>");
+
+//calcular el maximo de dos numeros
+var numero = Math.max(10, 20);
+document.write("El maximo entre 10 y 20 es: " + numero + "<br>");
+
+//calcular el minimo de dos numeros
+var numero = Math.min(10, 20);
+document.write("El minimo entre 10 y 20 es: " + numero + "<br>");
+
+//calcular el valor de PI
+var numero = Math.PI;
+document.write("El valor de PI es: " + numero + "<br>");
+
+//calcular el valor de E
+var numero = Math.E;
+document.write("El valor de E es: " + numero + "<br>");
+
+//calcular el valor de un numero elevado a otro
+var numero = Math.pow(2, 3);
+document.write("El valor de 2 elevado a 3 es: " + numero + "<br>");
+
+//Trear un numero aleatorio entre 0 y 1
+var numero = Math.random();
+document.write("El numero aleatorio es: " + numero + "<br>");
+
+//Trear un numero aleatorio entre 0 y 10
+var numero = Math.round(Math.random() * 10);
+document.write("El numero aleatorio entre 0 y 10 es: " + numero + "<br>");
+
+
+
