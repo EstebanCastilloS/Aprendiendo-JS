5c9b9c69931752337604c45aaa9b5ff9a8d8958b
EstebanCastillo <ncastillo308@unab.edu.co>
2024-01-31 07:46:44 -0500

    02_CAPTURAR DATOS

---
+++ 02_CLASES JS/01_PRACTICAS_YOUTUBE/02_CAPTURAR_DATOS/archivo.js
@@ -0,0 +1,10 @@
+document.write("Hola mundo parte 3 <br>");
+document.write("Adios mundo parte 3 <br>");
+
+
+let nombre, edad;
+nombre = prompt("Ingrese su nombre");
+edad = prompt("Ingrese su edad");
+
+document.write("Su nombre es: " + nombre + "<br>");
+document.write("Su edad es: " + edad + "<br>");
