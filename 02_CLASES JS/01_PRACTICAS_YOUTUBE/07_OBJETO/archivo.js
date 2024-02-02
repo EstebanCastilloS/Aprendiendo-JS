2bdc2cb6471309c19a6c822a699b2a2d102042db
EstebanCastillo <ncastillo308@unab.edu.co>
2024-02-02 14:51:32 -0500

    07_OBJETOS

---
+++ 02_CLASES JS/01_PRACTICAS_YOUTUBE/07_OBJETO/archivo.js
@@ -0,0 +1,18 @@
+var Auto = {
+    marca: 'Ford',
+    modelo: 'Fiesta',
+    anio: 2019,
+    color: 'rojo',
+    encendido: 'apagado',
+    tipo: 'hatchback',
+    radio: ['sony', 'pioneer', 'kenwood'],
+}
+
+document.write('Creación Objeto Auto <br>');
+document.write('Marca: ' + Auto.marca + '<br>');
+document.write('Modelo: ' + Auto.modelo + '<br>');
+document.write('Año: ' + Auto.anio + '<br>');
+document.write('Color: ' + Auto.color + '<br>');
+document.write('Encendido: ' + Auto.encendido + '<br>');
+document.write('Tipo: ' + Auto.tipo + '<br>');
+document.write('Radio: ' + Auto.radio + '<br>');
---
+++ 02_CLASES JS/01_PRACTICAS_YOUTUBE/08_CLASES/archivo.js
@@ -0,0 +1,2 @@
+document.write("Hola mundo parte 2 <br>");
+document.write("Adios mundo parte 2 <br>");
