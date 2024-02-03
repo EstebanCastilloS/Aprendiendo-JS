47b098c0935a2164adbd8714d7e970dccd5bd1fe
EstebanCastillo <ncastillo308@unab.edu.co>
2024-02-03 12:01:20 -0500

    13_CHECKBOX

---
+++ 02_CLASES JS/01_PRACTICAS_YOUTUBE/13_CHECKBOX/archivo.js
@@ -0,0 +1,73 @@
+function seleccion() {
+    let cantidad = 0;
+
+    if (document.getElementById("html").checked) {
+        cantidad++;
+    }
+    if (document.getElementById("css").checked) {
+        cantidad++;
+    }
+    if (document.getElementById("js").checked) {
+        cantidad++;
+    }
+    if (document.getElementById("php").checked) {
+        cantidad++;
+    }
+    if (document.getElementById("java").checked) {
+        cantidad++;
+    }
+    if (document.getElementById("python").checked) {
+        cantidad++;
+    }
+    if (document.getElementById("c").checked) {
+        cantidad++;
+    }
+    if (document.getElementById("c++").checked) {
+        cantidad++;
+    }
+    if (document.getElementById("c#").checked) {
+        cantidad++;
+    }
+    if (document.getElementById("ruby").checked) {
+        cantidad++;
+    }
+    if (document.getElementById("go").checked) {
+        cantidad++;
+    }
+    if (document.getElementById("swift").checked) {
+        cantidad++;
+    }
+    if (document.getElementById("kotlin").checked) {
+        cantidad++;
+    }
+    if (document.getElementById("typescript").checked) {
+        cantidad++;
+    }
+    if (document.getElementById("dart").checked) {
+        cantidad++;
+    }
+    if (document.getElementById("rust").checked) {
+        cantidad++;
+    }
+    if (document.getElementById("scala").checked) {
+        cantidad++;
+    }
+    if (document.getElementById("perl").checked) {
+        cantidad++;
+    }
+    if (document.getElementById("r").checked) {
+        cantidad++;
+    }
+    if (document.getElementById("sql").checked) {
+        cantidad++;
+    }
+    if (document.getElementById("bash").checked) {
+        cantidad++;
+    }
+    if (document.getElementById("powershell").checked) {
+        cantidad++;
+    }
+
+    alert("Cantidad de lenguajes seleccionados: " + cantidad);
+
+}
