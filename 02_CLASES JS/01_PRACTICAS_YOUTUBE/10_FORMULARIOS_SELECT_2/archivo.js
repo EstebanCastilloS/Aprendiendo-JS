0dfa9e0b2ec134ee333db0ac33739a5e5eca04b5
EstebanCastillo <ncastillo308@unab.edu.co>
2024-02-03 10:51:16 -0500

    12_FORMULARIO_2

---
+++ 02_CLASES JS/01_PRACTICAS_YOUTUBE/10_FORMULARIOS_SELECT_2/archivo.js
@@ -0,0 +1,7 @@
+
+function cambiarcolor(){
+    let seleccion = document.getElementById('select1');
+    document.getElementById('text1').value = seleccion.selectedIndex;
+    document.getElementById('text2').value = seleccion.options[seleccion.selectedIndex].text;
+    document.getElementById('text3').value = seleccion.options[seleccion.selectedIndex].value;
+}
