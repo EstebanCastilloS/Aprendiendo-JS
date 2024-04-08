3f1c1efcd62b96f265318d994d39473890814b0f
EstebanCastillo <ncastillo308@unab.edu.co>
2024-04-07 20:51:32 -0500

    Datos js

---
+++ 02_CLASES JS/02_PRACTICA_3/02_ASSIGNMENT/archivo_intro.js
@@ -0,0 +1,96 @@
+// JavaScript Assignment Operators
+
+// Operator	   Example	   Same As
+// =	       x = y	   x = y
+// +=	       x += y	   x = x + y
+// -=	       x -= y	   x = x - y
+// *=	       x *= y	   x = x * y
+// /=	       x /= y	   x = x / y
+// %=	       x %= y	   x = x % y
+// **=	       x **= y	   x = x ** y
+
+// ---------------------------------------------------------------
+
+//The = Operator
+//El operador de asignación simple asigna un valor a una variable.
+
+let x = 10;
+console.log(x);
+
+let y = 50
+let x1 = 10 + y;
+console.log(x1);
+
+// The += Operator
+// El operador de asignación de suma añade un valor a una variable.
+
+let x0 = 10;
+x0 += 5;
+console.log(x0);
+
+let text = "Hello";
+text += " World";
+console.log(text);
+
+// The -= Operator
+// El Operador de Asignación Sustracción sustrae un valor de una variable.
+
+let x2 = 10;
+x2 -= 5;
+console.log(x2);
+
+// The *= Operator
+// El Operador de Asignación Multiplicación multiplica una variable.
+
+let x3 = 10;
+x3 *= 5;
+console.log(x3);
+
+// The **= Operator
+// El operador de asignación de exponenciación eleva una variable a la potencia del operando.
+
+let x4 = 10;
+x4 **= 5;
+console.log(x4);
+
+// The /= Operator
+// El operador de asignación de división divide una variable.
+
+let x5 = 10;
+x5 /= 5;
+console.log(x5);
+
+// The %= Operator
+// El operador de asignación de resto asigna un resto a una variable.
+
+let x6 = 10;
+x6 %= 5;
+console.log(x6);
+
+
+
+
+
+// ---------------------------------------------------------------
+
+// Shift Assignment Operators
+// Operator	    Example	   Same As
+// <<=	        x <<= y	   x = x << y
+// >>=	        x >>= y	   x = x >> y
+// >>>=	        x >>>= y   x = x >>> y
+
+
+// Bitwise Assignment Operators
+// Operator	    Example	   Same As
+// &=	        x &= y	   x = x & y
+// ^=	        x ^= y	   x = x ^ y
+// |=	        x |= y	   x = x | y
+
+
+// Logical Assignment Operators
+// Operator	  Example	  Same As
+// &&=	      x &&= y	  x = x && (x = y)
+// ||=	      x ||= y	  x = x || (x = y)
+// ??=	      x ??= y	  x = x ?? (x = y)
+
+
