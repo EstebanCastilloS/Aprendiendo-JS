5f15c71cb321e1f41db2831ae5f359224bcfec8d
EstebanCastillo <ncastillo308@unab.edu.co>
2024-04-08 11:31:02 -0500

    Assigment js y Tipos de Datos js

--- 02_CLASES JS/02_PRACTICA_3/03_ASSIGNMENT/archivo_intro.js
+++ 02_CLASES JS/02_PRACTICA_3/03_ASSIGNMENT/archivo_intro.js
@@ -67,10 +67,6 @@ let x6 = 10;
 x6 %= 5;
 console.log(x6);
 
-
-
-
-
 // ---------------------------------------------------------------
 
 // Shift Assignment Operators
@@ -79,6 +75,30 @@ console.log(x6);
 // >>=	        x >>= y	   x = x >> y
 // >>>=	        x >>>= y   x = x >>> y
 
+// The <<= Operator
+// El operador de asignación de desplazamiento a la izquierda desplaza a la izquierda una variable.
+
+let x7 = -100;
+x7 <<= 5;
+console.log(x7);
+
+
+// The >>= Operator
+// El operador de asignación de desplazamiento a la derecha desplaza a la derecha una variable (con signo).
+
+let x8 = -100;
+x8 >>= 5;
+console.log(x8);
+
+// The >>>= Operator
+// El operador de asignación de desplazamiento a la derecha sin signo desplaza a la derecha 
+// una variable (sin signo).
+
+let x9 = -100;
+x9 >>>= 5;
+console.log(x9);
+
+//---------------------------------------------------------------
 
 // Bitwise Assignment Operators
 // Operator	    Example	   Same As
@@ -86,6 +106,33 @@ console.log(x6);
 // ^=	        x ^= y	   x = x ^ y
 // |=	        x |= y	   x = x | y
 
+// The &= Operator
+// El operador de asignación bit a bit AND realiza una operación bit a bit AND 
+// en dos operandos y asigna el resultado a la variable.
+
+let x10 = 100;
+x10 &= 5;
+console.log(x10);
+
+
+// The |= Operator
+// El operador de asignación Bitwise OR realiza una operación bitwise OR sobre dos operandos 
+// y asigna el resultado a la variable.
+
+let x11 = 10;
+x11 |= 5;
+console.log(x11);
+
+// The ^= Operator
+// El operador de asignación bit a bit XOR realiza una operación bit a bit XOR 
+// en dos operandos y asigna el resultado a la variable.
+
+let x12 = 10;
+x12 ^= 5;
+console.log(x12);
+
+
+// ---------------------------------------------------------------
 
 // Logical Assignment Operators
 // Operator	  Example	  Same As
@@ -93,4 +140,29 @@ console.log(x6);
 // ||=	      x ||= y	  x = x || (x = y)
 // ??=	      x ??= y	  x = x ?? (x = y)
 
+// The &&= Operator
+// El operador lógico de asignación AND se utiliza entre dos valores.
+// Si el primer valor es verdadero, se asigna el segundo valor.
 
+let x13 = 10;
+x13 &&= 5;
+console.log(x13);
+
+// The ||= Operator
+// El operador lógico de asignación OR se utiliza entre dos valores.
+// Si el primer valor es falso, se asigna el segundo valor.
+
+let x14 = 10;
+x14 ||= 5;
+console.log(x14);
+
+// The ??= Operator
+// El operador de asignación de coalescencia nulo se utiliza entre dos valores.
+// Si el primer valor es indefinido o nulo, se asigna el segundo valor.
+
+let x15;
+x15 ??= 5;
+console.log(x15);
+
+
+// ---------------------------------------------------------------
